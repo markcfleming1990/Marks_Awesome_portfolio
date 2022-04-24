58f7abe7dcfd3ee0c096a6f1b90dd55c2d2076ba
Mark <markcfleming1990@gmail.com>
2022-04-24 11:06:42 -0400

    worked on about section

---
+++ marks-portfolio/src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Hi! My name is Mark!</h1>
+      <div>
+        <p>
+          Welcome to my page! My name is Mark Fleming, a career Sales professional turned Web Developer living with my wife Coleen in Goshen Indiana.
+          I started my journey into Web developing in order to change careers after I obtain my Full Stack Devoplment certificate from Bulter University's
+          Coding Bootcamp in May 2022. My sales experience brings a compeititive and professional spirit to any team.
+          </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
